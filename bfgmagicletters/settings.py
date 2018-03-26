4045b9e2e4d30138531cbd717eee801398cee24a
kst123abc <kylesuero@gmail.com>
2018-03-26 16:03:55 -0400

    fixed naming

--- bfgmagicletters/settings.py
+++ bfgmagicletters/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'letters.apps.LettersConfig',
+    'letter.apps.LettersConfig',
 ]
 
 MIDDLEWARE = [
--- letter/apps.py
+++ letter/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class LettersConfig(AppConfig):
-    name = 'letters'
+    name = 'letter'
