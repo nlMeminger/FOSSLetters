5b50d3d00188e80d5f3e43fac6d6ebed60e007e9
kst123abc <kylesuero@gmail.com>
2018-03-26 15:45:26 -0400

    added letters model

--- bfgmagicletters/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Letters(models.Model):
-    letter = models.CharField(max_length=1)
-    currrent_color = models.CharField(max_length=6)
-    default_color = models.CharField(max_length=6)
--- letters/models.py
+++ letters/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Letters(models.Model):
+    letter = models.CharField(max_length=1)
+    currrent_color = models.CharField(max_length=6)
+    default_color = models.CharField(max_length=6)
