687cd818422be1ab62609eb54305ed69c930d302
Dan Schneiderman <schneiderman.daniel@gmail.com>
2018-03-26 16:13:51 -0400

    Added migrations and def for letter name

--- letter/models.py
+++ letter/models.py
@@ -6,3 +6,6 @@ class Letter(models.Model):
     letter = models.CharField(max_length=1)
     currrent_color = models.CharField(max_length=6)
     default_color = models.CharField(max_length=6)
+
+    def __unicode__(self):
+       return self.letter
