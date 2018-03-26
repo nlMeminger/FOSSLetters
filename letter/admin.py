eb5a2d2117ad7d9f1f0e19ac6c3e524f4b71b07d
Dan Schneiderman <schneiderman.daniel@gmail.com>
2018-03-26 16:09:02 -0400

    Fixed letters to letter

--- letter/admin.py
+++ letter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Letters
+from .models import Letter
 
-admin.site.register(Letters)
+admin.site.register(Letter)
--- letter/models.py
+++ letter/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Letters(models.Model):
+class Letter(models.Model):
     letter = models.CharField(max_length=1)
     currrent_color = models.CharField(max_length=6)
     default_color = models.CharField(max_length=6)
