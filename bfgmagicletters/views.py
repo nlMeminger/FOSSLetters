52cfd92e01ff84e9e6cfdb993eb54d5f048ff8d8
Dan Schneiderman <schneiderman.daniel@gmail.com>
2018-03-26 17:00:42 -0400

    Default colors. Woo.

--- bfgmagicletters/views.py
+++ bfgmagicletters/views.py
@@ -3,4 +3,8 @@ from letter.models import Letter
 
 def home(request):
 	letter_M = Letter.objects.get(letter='M')
-	return render(request, 'letters.html', {'letter': letter_M})
+	letter_A = Letter.objects.get(letter='A')
+	letter_G = Letter.objects.get(letter='G')
+	letter_I = Letter.objects.get(letter='I')
+	letter_C = Letter.objects.get(letter='C')
+	return render(request, 'letters.html', {'M': letter_M, 'A': letter_A, 'G': letter_G, 'I': letter_I, 'C': letter_C})
