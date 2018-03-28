c83c095257aa64ab50fd6a0bf50098f100ccbf3c
Dan Schneiderman <schneiderman.daniel@gmail.com>
2018-03-28 13:51:20 -0400

    Fixed inital opc code

--- bfgmagicletters/views.py
+++ bfgmagicletters/views.py
@@ -27,21 +27,22 @@ def home(request):
 	speed_g = -13
 	speed_b = 19
 
-	start_time = time.time()
-	while True:
-            t = (time.time() - start_time) * 5
-            pixels = []
-            for ii in range(n_pixels):
-                pct = (ii / n_pixels)
-                # diagonal black stripes
-                pct_jittered = (pct * 77) % 37
-                blackstripes = color_utils.cos(pct_jittered, offset=t*0.05, period=1, minn=-1.5, maxx=1.5)
-                blackstripes_offset = color_utils.cos(t, offset=0.9, period=60, minn=-0.5, maxx=3)
-                blackstripes = color_utils.clamp(blackstripes + blackstripes_offset, 0, 1)
-                # 3 sine waves for r, g, b which are out of sync with each other
-                r = blackstripes * color_utils.remap(math.cos((t/speed_r + pct*freq_r)*math.pi*2), -1, 1, 0, 256)
-                g = blackstripes * color_utils.remap(math.cos((t/speed_g + pct*freq_g)*math.pi*2), -1, 1, 0, 256)
-                b = blackstripes * color_utils.remap(math.cos((t/speed_b + pct*freq_b)*math.pi*2), -1, 1, 0, 256)
-                pixels.append((r, g, b))
-        client.put_pixels(pixels, channel=0)
-        time.sleep(1 / fps)
+	pixels = []
+	for ii in range(n_pixels):
+		pct = (ii / n_pixels)
+		# diagonal black stripes
+		# pct_jittered = (pct * 77) % 37
+		# blackstripes = color_utils.cos(pct_jittered, offset=t*0.05, period=1, minn=-1.5, maxx=1.5)
+		# blackstripes_offset = color_utils.cos(t, offset=0.9, period=60, minn=-0.5, maxx=3)
+		# blackstripes = color_utils.clamp(blackstripes + blackstripes_offset, 0, 1)
+		# # 3 sine waves for r, g, b which are out of sync with each other
+		# r = blackstripes * color_utils.remap(math.cos((t/speed_r + pct*freq_r)*math.pi*2), -1, 1, 0, 256)
+		# g = blackstripes * color_utils.remap(math.cos((t/speed_g + pct*freq_g)*math.pi*2), -1, 1, 0, 256)
+		# b = blackstripes * color_utils.remap(math.cos((t/speed_b + pct*freq_b)*math.pi*2), -1, 1, 0, 256)
+		r = 255
+		g = 0
+		b = 0
+		pixels.append((r, g, b))
+	client.put_pixels(pixels, channel=0)
+
+	return render(request, 'letters.html', {'M': letter_M, 'A': letter_A, 'G': letter_G, 'I': letter_I, 'C': letter_C})
